6d3695a9a2baa8f2565b9d5521c05fc240305722
Juan Mendez <contact@juanmendez.info>
2017-06-02 08:37:00 -0500

    renamed module with fragmentNavigator

---
+++ fragmentNavigator/src/main/java/info/juanmendez/fragmentnavigator/FragmentNavigator.java
@@ -0,0 +1,11 @@
+package info.juanmendez.fragmentnavigator;
+
+/**
+ * Created by Juan Mendez on 6/2/2017.
+ * www.juanmendez.info
+ * contact@juanmendez.info
+ */
+
+public class FragmentNavigator {
+
+}
